8bd24ac223ae21aa2e03c94f4c5baa1121d5d201
berfinduru <berfinduru99@outlook.com>
2022-07-19 12:37:43 +0300

    lol

--- mynew.py
+++ mynew.py
@@ -1,8 +1,12 @@
 from flask import Flask
+#from dotenv import load_dotenv 
+#load_dotenv() 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return f'Hello, {os.getenv("SECRET_NAME")}, the menaing of life is {os.getenv("MEANING_OF_LIFE")}'
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
